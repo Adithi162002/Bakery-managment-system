bd55f7432e7662f5671ecb15c362e5d58cca0503
Adithi Gowda <116082380+Adithi162002@users.noreply.github.com>
2023-03-23 16:38:06 +0530

    model files

---
+++ model/Cake.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author POULAMI
+ */
+public class Cake {
+    private int id;
+    private String flavour;
+    private String color;
+    private String weight;
+    private String shape;
+    private String allergetic;
+    private String egg_or_eggless;
+    private String ocassion;
+    private String name_on_cake;
+    private String dod;
+    private String price;
+    private String customername;
+    private String mobilenumber;
+
+     public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFlavour() {
+        return flavour;
+    }
+
+    public void setFlavour(String flavour) {
+        this.flavour = flavour;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+    
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+    
+    public String getAllergetic() {
+        return allergetic;
+    }
+
+    public void setAllergetic(String allergetic) {
+        this.allergetic = allergetic;
+    }
+    
+    public String getEggoreggless() {
+        return egg_or_eggless;
+    }
+
+    public void setEggoreggless(String egg_or_eggless) {
+        this.egg_or_eggless = egg_or_eggless;
+    }
+    
+    public String getOcassion() {
+        return ocassion;
+    }
+
+    public void setOcassion(String ocassion) {
+        this.ocassion = ocassion;
+    }
+    
+    public String getNameoncake() {
+        return name_on_cake;
+    }
+
+    public void setNameoncake(String name_on_cake) {
+        this.name_on_cake = name_on_cake;
+    }
+    
+    public String getDod() {
+        return dod;
+    }
+
+    public void setDod(String dod) {
+        this.dod = dod;
+    }
+    
+    public String getCustomername() {
+        return customername;
+    }
+
+    public void setCustomername(String customername) {
+        this.customername = customername;
+    }
+    public String getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public void setMobilenumber(String mobilenumber) {
+        this.mobilenumber = mobilenumber;
+    }
+}
